7433343f9f28ddb335e81d9ec9ce2b834e16d76b
steve <stevenlarry1710@gmail.com>
2026-01-19 21:32:09 +0100

    no api key leak

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,14 +1,28 @@
 from openai import OpenAI
 from secret import OPENAI_KEY
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 client = OpenAI(
     api_key=OPENAI_KEY
 )
 
-response = client.responses.create(
-    model="gpt-5 nano",
-    input=""
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
-print(response.output_text)
+class ChatRequest(BaseModel):
+    messages: list[dict]
+
+def chat(req:ChatRequest): 
+    resp = client.responses.create(
+        model="gpt-4.1-mini",
+        input=req.messages
+    )
+    return resp.output_text or ""
